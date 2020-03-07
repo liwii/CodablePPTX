8feb5993fd46a2c94575caed9a50c66b02cd733e
Koki Ryu <liukoki@gmail.com>
2020-03-07 17:37:58 -0500

    watch file change

--- presentation.sh
+++ presentation.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
-while true
-do
-python presentation.py $1 $2
-soffice --headless --convert-to pdf $2
-BASE=$2
-md5sum ${BASE%.*}.pdf > ${BASE%.*}.md5
-sleep 5
-done
 
+fswatch -0 ./ | while read -d "" event
+do
+  E=${event}
+  EXT=${E##*.}
+  FILE=${E##*/}
+  if test $EXT = "yaml" || test $EXT = "yml"; then
+    echo $FILE changed
+    python presentation.py $1 $2
+    soffice --headless --convert-to pdf $2
+    BASE=$2
+    md5sum ${BASE%.*}.pdf > ${BASE%.*}.md5
+    echo $2 generated
+  fi
+done
